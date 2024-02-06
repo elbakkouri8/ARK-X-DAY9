2a478b5acc739a24372a722f78347d2927ad0315
elbakkouri8 <58864304+elbakkouri8@users.noreply.github.com>
2024-02-06 19:09:42 +0100

    Add files via upload

---
+++ DAY11/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String s1="hello";
+        String s2="lloeh";
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
